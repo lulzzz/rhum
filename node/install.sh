d7849294e5a6a7f533b80ff85323baf23de34bb5
root <root@rhum-mk1>
2016-06-25 14:49:21 -0400

    added mcgill.conf info for wpa and interfaces.d

--- node/install.sh
+++ node/install.sh
@@ -15,6 +15,7 @@ if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
         apt-get update
         apt-get upgrade -y -qq
         apt-get install firmware-realtek firmware-iwlwifi -y -qq
+        apt-get install wireless-tools -y -qq
         apt-get install unzip -y -qq
         apt-get install build-essential -y -qq
         apt-get install python-dev -y -qq
