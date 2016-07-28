04f6e27edac4b5734a4619fbd02a1cc0e70b2ce3
root <root@rhum-mk1>
2016-07-28 18:52:05 -0400

    moved nomenclature to server-manager-node

--- manager/install.sh
+++ manager/install.sh
@@ -1,13 +1,64 @@
 #!/bin/sh
-apt-get update
-apt-get install mongodb git python-pip apache2 libapache2-mod-wsgi build-essential python-dev -qq -y --fix-missing 
-a2enmod wsgi
-pip install flask virtualenv pymongo
-cp manager/rhum.conf /etc/apache2/sites-available/
-a2ensite rhum.conf
-a2dissite 000-default.conf
+# RHUM Node Installer
+# WARNING: This software makes significant changes to the system behavior
+# DISCLAIMER: This software is distributed with no warranty.
 
-# Update Kernel
-read -p "Enter host IP address: ? " ans
-echo $ans >> /etc/hosts
-service apache2 restart
+#
+cp -r libraries/* /usr/share/arduino/libraries
+cp conf/mcgill.cfg /etc/network/interfaces.d/
+cp conf/mcgill.conf /etc/wpa_supplicant/
+
+# APT Packages
+read -p "Update APT dependencies? [y/n] " ans
+if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
+    then
+        echo "Installing dependencies via mirror ..."
+        cp conf/sources.list /etc/apt/ # includes NON-FREE repo
+        apt-get update
+        apt-get upgrade -y -qq
+        apt-get install firmware-realtek firmware-iwlwifi -y -qq
+        apt-get install wireless-tools -y -qq
+        apt-get install unzip -y -qq
+        apt-get install build-essential -y -qq
+        apt-get install python-dev -y -qq
+        apt-get install cmake -y -qq
+        apt-get install python-serial -y -qq
+        apt-get install python-pip -y -qq
+        apt-get install python-matplotlib -y -qq
+        apt-get install libgtk2.0-dev -y -qq
+        apt-get install python-numpy -y -qq
+        apt-get install arduino arduino-mk -qq
+        apt-get install x11-xserver-utils -y -qq
+        apt-get install cython -y  -qq
+	apt-get install python-flask -y -qq
+	apt-get install python-requests -y -qq
+fi
+if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
+    then
+        echo "Aborting..."
+fi
+
+# Pip Packages
+read -p "Update Pip modules? [y/n] " ans
+if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
+    then
+        echo "Installing Python modules..."
+        pip install -r requirements.txt
+fi
+if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
+    then
+        echo "Aborting..."
+fi
+
+# Done Message
+read -p "Installation Complete! Reboot now? [y/n] " ans
+if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
+    then
+    echo "Rebooting..."
+    reboot
+fi
+if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
+    then
+        echo "Aborting..."
+fi
+
