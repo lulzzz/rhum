459c6da2951f6f1f5408568b61b725ebcb0a6881
trevstanhope <tpstanhope@gmail.com>
2016-09-05 08:05:29 -0400

    added read serial

--- manager/tests/read_serial.py
+++ manager/tests/read_serial.py
@@ -4,19 +4,14 @@ import ast
 import time
 
 class Arduino:
-    def __init__(self, dev='/dev/ttyACM0', baud=9600):
+    def __init__(self, dev='/dev/ttyACM0', baud=38400):
         self.port = serial.Serial(dev, baud)
 
     def listen(self):
         s = self.port.readline()
         return s
-    
-    def send_command(self, cmd, val):
-        out = str(cmd) + str(val) + '\n'
-        self.port.write(out)
 
 if __name__ == '__main__':
-    d = raw_input('Enter the device name: ')
     app = Arduino(dev=d)
     while True:
         try:
