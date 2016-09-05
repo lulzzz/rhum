f991af90ca8566d45989989e2758818a7ce7cfca
trevstanhope <tpstanhope@gmail.com>
2016-09-05 08:02:38 -0400

    added read serial

--- manager/tests/read_serial.py
+++ manager/tests/read_serial.py
@@ -10,31 +10,6 @@ class Arduino:
     def listen(self):
         s = self.port.readline()
         return s
-
-    def parse(self, s):
-        try:
-            msg = ast.literal_eval(s)
-            pid = msg['pid']
-            chksum1 = msg['chksum']
-            data = msg['data']
-            chksum2 = self.checksum(data)
-            if chksum1 == chksum2 :
-                return data
-            else:
-                return None
-        except Exception as e:
-            raise e
-    
-    def checksum(self, data):
-        chksum = 0
-        try:
-            s = str(data)
-            s_clean = s.replace(' ', '')
-            for i in s_clean:
-                chksum += ord(i)
-            return chksum % 256
-        except Exception as e:
-            raise e
     
     def send_command(self, cmd, val):
         out = str(cmd) + str(val) + '\n'
@@ -45,10 +20,8 @@ if __name__ == '__main__':
     app = Arduino(dev=d)
     while True:
         try:
-            a = app.listen()
-            print a
-            res = app.parse(a)
-            print res
+            s = app.listen()
+            print s
         except KeyboardInterrupt:
             break
         except Exception as e:
