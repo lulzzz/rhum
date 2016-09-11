e35f73a6eaf7aaf46da02750d3911e3b583adb1f
trevstanhope <tpstanhope@gmail.com>
2016-09-11 14:08:55 -0400

    changed layout of app

--- manager/ionic/www/js/controllers.js
+++ manager/ionic/www/js/controllers.js
@@ -3,26 +3,19 @@ angular.module("controllers", [])
 // Basic Settings
 .controller('DashCtrl', function($scope, $http, dashSettings, host_url) {
     $scope.Days = {name: "Days", value:1};
-    $scope.setRange = function () {
+    $scope.getCSV = function () {
         $http.post("regen/" + $scope.Days.value, {}).then(function (res) {
             alert( "Manager received request to generate CSV! This operation may take a moment ...");
         });
-    }
-    $scope.getCSV = function () {
+        setTimeout(function(){}, 5000);
+        alert( "Generated CSV! ...");
         window.location = "http://" + host_url + "/logs/data-" + $scope.Days.value + ".csv";
-    }
-})
-
-.controller('AboutCtrl', function($scope, $http, aboutSettings, host_url) {
-    $scope.VERBOSE = false;
-    $scope.setVerbose = function() {
-        if ($scope.VERBOSE == true) {
-            $scope.VERBOSE = false;
-        } else {
-            $scope.VERBOSE = true;
-        }
     }
     $scope.getErrors = function () {
         window.location = "http://" + host_url + "/logs/errors.txt";
     }
+})
+
+.controller('AboutCtrl', function($scope, $http, aboutSettings, host_url) {
+    // No functions for About (yet)
 });
