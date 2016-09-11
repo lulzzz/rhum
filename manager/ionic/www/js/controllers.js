b3567c5c48c6b45972e9239fd1f2639021393c95
trevstanhope <tpstanhope@gmail.com>
2016-09-11 18:54:33 -0400

    changed js functions

--- manager/ionic/www/js/controllers.js
+++ manager/ionic/www/js/controllers.js
@@ -3,11 +3,11 @@ angular.module("controllers", [])
 // Basic Settings
 .controller('DashCtrl', function($scope, $http, dashSettings, host_url) {
     $scope.Days = {name: "Days", value:1};
-    $scope.getCSV = function () {
+    $scope.regenCSV = function () {
         $http.post("regen/" + $scope.Days.value, {}).then(function (res) {});
         alert( "Manager received request to generate CSV! This operation may take a moment.");
     }
-    $scope.regenCSV = function () {
+    $scope.getCSVregenCSV = function () {
         window.location = "http://" + host_url + "/logs/data-" + $scope.Days.value + ".csv";
     }
     $scope.getErrors = function () {
